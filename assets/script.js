c6de57a019c49a424be730747dcc8c331b49528e
tracy rose guajardo <tracyroseguajardo@gmial.com>
2022-05-22 16:27:25 -0700

    refactoring. But still not able to move to next question

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,18 @@
+/* GLOBAL VARIABLES */
 // Store Questions as Objects
 var questions = [
   {
-    Question: "D???",
+    Question: "A???",
     choices: ['A', 'b', 'c', 'd', 'f', 'e'],
     Answer: "c",
   },
   {
-    Question: "D???",
+    Question: "B???",
     choices: ['A', 'b', 'c', 'd'],
     Answer: "A",
   },
   {
-    Question: "D???",
+    Question: "C???",
     choices: ['A', 'b', 'c', 'd'],
     Answer: "b",
   },
@@ -21,32 +22,32 @@ var questions = [
     Answer: "d",
   },
   {
-    Question: "D???",
+    Question: "E???",
     choices: ['A', 'b', 'c', 'd', 'f', 'e'],
     Answer: "c",
   },
   {
-    Question: "D???",
+    Question: "F???",
     choices: ['A', 'b', 'c', 'd', 'f', 'e'],
     Answer: "c",
   },
   {
-    Question: "D???",
+    Question: "G???",
     choices: ['A', 'b', 'c', 'd', 'f', 'e'],
     Answer: "c",
   },
   {
-    Question: "D???",
+    Question: "H???",
     choices: ['A', 'b', 'c', 'd', 'f', 'e'],
     Answer: "c",
   },
   {
-    Question: "D???",
+    Question: "I???",
     choices: ['A', 'b', 'c', 'd', 'f', 'e'],
     Answer: "c",
   },
   {
-    Question: "D???",
+    Question: "J???",
     choices: ['A', 'b', 'c', 'd', 'f', 'e'],
     Answer: "c",
   },
@@ -61,111 +62,133 @@ var quizEl = document.getElementById("quiz");
 var timeEl = document.querySelector("#time");
 var timerInterval;
 var score = 0
-var currentQuestionObj = questions[index];
+//var currentQuestionObj = questions[index];
 
+/* DECLARED FUNCTIONS */
 function start() {
-
+  //immediately shows time remaining upon start with no delay
   timeEl.textContent = secondsLeft + " seconds remaining of quiz."
   // when the user clicks start we need to start the timer
-  timerInterval = setInterval(function() {
+  timerInterval = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = secondsLeft + " seconds remaining of quiz.";
-
-    //In case time runs out before quiz completed
-    if (secondsLeft === 0) {
-      // clearInterval(timerInterval);
-      // sendMessage();
-      //return quiz
-    }
-
-    // //Commented out bc not fully completed
-    // //In case a question is answered wrong
-    // if(response !== answer) {
-    //   secondsLeft - 10;
-    // }, 1000;
-
   }, 1000);
 
   // hide the starter container
   startEl.setAttribute("class", "hidden");
   // unhide the quiz container
   quizEl.removeAttribute("class");
-  
-  // function sendMessage() {
-  //   timeEl.textContent = "TIME OUT";
-  // }
 
-  // Appends active question to HTML quiz div
-
-// FUNCTION TO GET QUESTIONS
+  // FUNCTION TO GET QUESTIONS
   quizSequence();
-
 }
 
 function quizSequence() {
-// get the current question object from the array
-//var currentQuestionObj = questions[index];
+  // get the current question object from the array
+  var currentQuestionObj = questions[index];
+  //  create elment to display our question title
+  var questionTitle = document.createElement("h3");
+  questionTitle.textContent = currentQuestionObj.Question;
 
-//  create elment to display our question title
-var questionTitle = document.createElement("h3");
-questionTitle.textContent = currentQuestionObj.Question;
+  // create element to house the buttons
+  var buttonDiv = document.createElement("div");
 
-// create element to house the buttons
-var buttonDiv = document.createElement("div");
+  // create button dynamically based off of choice array in question variable
+  for (var i = 0; i < currentQuestionObj.choices.length; i++) {
+    // create buttons
+    var btn = document.createElement("button");
+    btn.textContent = currentQuestionObj.choices[i];
+    btn.setAttribute('value', currentQuestionObj.choices[i])
+    //add event listener to buttons
+    // btn.addEventListener("click", function() {
+    //   btn.value = checkAnswer
+    // });
+    btn.onclick = checkAnswer;
 
-// create button dynamically based off of choice array in question variable
-for (var i = 0; i < currentQuestionObj.choices.length; i++) {
-  // create buttons
-  var btn = document.createElement("button");
-  btn.textContent = currentQuestionObj.choices[i];
-  btn.setAttribute('value', currentQuestionObj.choices[i])
-  //add event listener to buttons
-  // btn.addEventListener("click", function() {
-  //   btn.value = checkAnswer
-  // });
-  btn.onclick = checkAnswer;
+    // add buttons to container that house buttons
+    buttonDiv.append(btn);
+  }
 
-  // add buttons to container that house buttons
-  buttonDiv.append(btn);
+  // add the elements to quizEl
+  quizEl.append(questionTitle, buttonDiv);
+  // }
+
+  function checkAnswer() {
+    console.log(this.value);
+    //console.log(event.target.value);
+    // check if the answer is wrong
+    console.log(currentQuestionObj.Answer);
+    if (this.value != currentQuestionObj.Answer) {
+      // deduct 11 sec from the time
+      secondsLeft = (secondsLeft - 10);
+      timeEl.textContent = secondsLeft + " seconds remaining of quiz.";
+    } else {
+      //increase score +1  
+      score = (score + 1);
+      console.log(score);
+    }
+    // // increase the index by 1
+    questions[i++];
+    console.log(questions[i++]);
+    //nextQuestion();
+  }
+
+  // if there are no more questions in the array end the game else ask the next question
+  // for (var i = 0; i < (currentQuestionObj); i++) {
+  //   console.log(currentQuestionObj);
+  //   questionTitle.textContent = currentQuestionObj.Question;
+  // }
+
+
+  // // function nextQuestion() {
+  //   for (var i = 0; i < (currentQuestionObj); i++) {
+  //   console.log(currentQuestionObj);
+  //   questionTitle.textContent = currentQuestionObj.Question;
+  // }
+  //   if (currentQuestionObj.Question[i++]) {
+  //questionTitle.textContent = currentQuestionObj.Question[i++];
+  // console.log(currentQuestionObj.Question[i++])
+  //}
+  // }
+
+  //In case time runs out before quiz completed
+  if (secondsLeft === 0) {
+    clearInterval(timerInterval);
+    //sendMessage();
+    //return quiz
+  }
 }
-
-// add the elements to quizEl
-quizEl.append(questionTitle, buttonDiv);
-}
-
-function checkAnswer(){
-  console.log(this.value);
-  //console.log(event.target.value);
-  // check if the answer is wrong
-  console.log(currentQuestionObj.Answer);
-  if (this.value != currentQuestionObj.Answer) {
-  // deduct 11sec from the time
-  secondsLeft = (secondsLeft - 11);
-  timeEl.textContent = secondsLeft + " seconds remaining of quiz.";
-  } else {
-  // // increase the index by 1
-  // (response + 1)
-  score = (score + 1);
-  console.log(score);
-  } 
-}
-
-// if there are no more questions in the array end the game else ask the next question
-// currentQuestionObj.question[i++];
-
-// }
 
 // create function that ends the game and stops your timer, hides the wquestion container and displays the game over container
 
+/* EVENT LISTENERS */
 startQuiz.addEventListener("click", start);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // TOP: any global variables
 
 // MIDDLE: any functions that are declared
 
 // BOTTOM:  all event listeners
-
-
 
 // For Argument to get quiz questions to rotate through
 // alreadyAsked = []
@@ -182,3 +205,19 @@ startQuiz.addEventListener("click", start);
 // function randomIndex(array) {
 //   return array[Math.floor(Math.random() * array.length)];
 // }
+
+  //In case time runs out before quiz completed
+  // if (secondsLeft === 0) {
+    // clearInterval(timerInterval);
+    // sendMessage();
+    //return quiz
+  // }
+
+    // function sendMessage() {
+  //   timeEl.textContent = "TIME OUT";
+  // }
+
+  // Appends active question to HTML quiz div
+
+    //for loop to pass through all questions
+    // for (var i = 0; i < questions.length; i++){}
